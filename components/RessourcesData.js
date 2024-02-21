dafbe61032c560c8fdf478d3747e7a5b6ca15ebb
marvinescalle <77961839+marvinescalle@users.noreply.github.com>
2024-02-21 19:18:58 +0100

    Add files via upload

---
+++ components/RessourcesData.js
@@ -0,0 +1,52 @@
+export const Ressources = [
+    {
+        id:1,
+        name:"IA",
+        description:"Je m'intéresse beaucoup aux actualités concernant les intelligences artificielles, j'en utilise beaucoup que ce soit en ligne ou en local, je me sers d'IA Open Source pour m'entrainer sur divers domaines et/ou générer du contenu utile pour mes passions",
+        tags:[""],
+        
+        demo:"",
+    },
+    {
+        id:2,
+        name:"Informatique",
+        description:"Je m'intéresse beaucoup également à l'informatique en général, j'aime monter des PC moi même, designer des sites web, m'informer sur l'actualité technologique. Je suis curieux et l'informatique est un monde vaste qui m'intéresse beaucoup.",
+        tags:[""],
+        
+        demo:"",
+        
+    },
+    {
+        id:3,
+        name:"Art",
+        description:"Je m'intéresse à l'art sous toutes ses formes, j'ai pour cela appris au collège à jouer de la guitare mais aussi à dessiner, durant mon temps libre je peux passer plusieurs heures ou jours sur le même dessin. J'ai déjà remporté quelques petits concours non officiels de dessins sur internet.",
+        tags:["instagram : @marmar_drw"],
+        
+        demo:"",
+    },
+    {
+        id:4,
+        name:"Athlétisme",
+        description:"J'ai pratiqué l'athlétisme pendant plusieurs années, j'ai été durant mes années de lycée Champion Départemental 13 de sprint 100m ainsi que Vice champion Régional PACA de Sprint relais 400m. J'ai supervisé quelques séances d'athlétisme pour des petits groupes de benjamins (environ 12 ans). J'adore courir, sprinter plus particulièrement.",
+        tags:[""],
+        
+        demo:"",
+    },
+    {
+        id:5,
+        name:"Sports de combats",
+        description:"Je pratique depuis peu de temps de la Boxe Thaïlandaise ainsi que du MMA. Je suis beaucoup également les combats professionnels sur internet.",
+        tags:[""],
+        
+        demo:"",
+    },
+    {
+        id:6,
+        name:"Théâtre",
+        description:"Je fais partie d'un petit groupe de théâtre d'improvisation, tous les mardis nous prenons des cours avec des professionnels, cela m'entraine à mieux m'exprimer, prendre confiance en moi devant un public...",
+        tags:[""],
+        
+        demo:"",
+    },
+  
+] 
